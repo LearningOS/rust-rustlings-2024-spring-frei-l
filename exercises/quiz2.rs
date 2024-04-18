8de5330cd410827e740d37b82d2c645477e4c5b3
freil <116133704+frei-l@users.noreply.github.com>
2024-04-18 09:04:58 +0000

    update

--- exercises/quiz2.rs
+++ exercises/quiz2.rs
@@ -32,7 +32,7 @@ mod my_module {
     use super::Command;
 
     // TODO: Complete the function signature!
-    pub fn transformer(input: ???) -> ??? {
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
         // TODO: Complete the output declaration!
         let mut output: ??? = vec![];
         for (string, command) in input.iter() {
@@ -45,7 +45,7 @@ mod my_module {
 #[cfg(test)]
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
-    use ???;
+    use transformer;
     use super::Command;
 
     #[test]
